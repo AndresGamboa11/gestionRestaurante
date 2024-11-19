621d3427ff68da3b7fba57e773eff5062304a712
AndresGamboa11 <erickcarman98@gmail.com>
2024-11-18 22:25:10 -0500

    commit estilos de la vista home

--- resources/views/auth/Reservas.blade.php
+++ resources/views/auth/Reservas.blade.php
@@ -7,19 +7,14 @@
     <link rel="stylesheet" href="css/home.css">
 </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="{{ route('reservas') }}">Reservas</a></li>
-                <li><a href="{{ route('pedidos') }}">Pedidos</a></li>
-                <li><a href="{{ route('inventario') }}">Inventario</a></li>
-                <li>
-                    <form action="{{ route('logout') }}" method="POST">
-                        @csrf
-                        <button type="submit">Cerrar Sesión</button>
-                    </form>
-                </li>
-            </ul>
+    <header class="header">
+        <h2 class="logo">Proyecto</a></h1>
+            <p class="nombreRestaurante">Restaurante Colombiano</p>
+        <nav class="cerrarSesion">
+           <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button class="botonT" type="submit">Cerrar Sesión</button>
+            </form>
         </nav>
     </header>
     <main>
